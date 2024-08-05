9ac7d882942f81cc4d2800561e24fc922a4a6237
Sergei Ryzhko <ethrai@proton.me>
2024-08-06 00:02:12 +0300

    add variable specifying which code to use

--- .nixos/development/vscode.nix
+++ .nixos/development/vscode.nix
@@ -5,16 +5,31 @@
   inputs,
   ...
 }:
-
+let
+  homeDir = config.home.homeDirectory;
+  ofSym = config.lib.file.mkOutOfStoreSymlink;
+  code = "Code";
+in
 {
-  stylix.targets.vscode.enable = false;
-  programs.vscode = {
-    package = pkgs.vscodium;
-    enable = true;
-    extensions = with pkgs.vscode-extensions; [
-      vscodevim.vim
-      yzhang.markdown-all-in-one
-      jnoortheen.nix-ide
-    ];
+  options = { };
+  config = {
+    stylix.targets.vscode.enable = false;
+    programs.vscode = {
+      package = pkgs.vscode;
+      enable = true;
+      extensions = with pkgs.vscode-extensions; [
+        vscodevim.vim
+        yzhang.markdown-all-in-one
+        jnoortheen.nix-ide
+
+        mads-hartmann.bash-ide-vscode
+      ];
+    };
+
+    home.file = {
+      ".config/${code}/User/settings.json".source = ofSym "${homeDir}/.dots/.config/VSCodium/User/settings.json";
+      ".config/${code}/User/keybindings.json".source = ofSym "${homeDir}/.dots/.config/VSCodium/User/keybindings.json";
+    };
+
   };
 }
