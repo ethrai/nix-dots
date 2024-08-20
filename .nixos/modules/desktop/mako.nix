6279d8da9641a84168513a6c66b6c6a26841326b
Sergei Ryzhko <ryzhkosergej@gmail.com>
2024-08-20 14:53:51 +0300

    dunst -> mako

--- .nixos/modules/desktop/mako.nix
+++ .nixos/modules/desktop/mako.nix
@@ -1,24 +1,25 @@
 { config, ... }: {
   stylix.targets.mako.enable = false;
   services = {
-    mako = {
+    mako = with config.lib.stylix.colors; {
       enable = true;
       font = "${config.stylix.fonts.monospace.name} 15";
       padding = "15";
       defaultTimeout = 5000;
       borderSize = 2;
       borderRadius = 5;
-      backgroundColor = "#1e1e2e";
-      borderColor = "#b4befe";
-      progressColor = "over #313244";
-      textColor = "#cdd6f4";
+      backgroundColor = "${base00}";
+      borderColor = "#${base07}";
+      progressColor = "over #${base02}";
+      textColor = "#${base05}";
       icons = true;
       actions = true;
+      anchor = "bottom-right";
       extraConfig = ''
         text-alignment=center
         [urgency=high]
         default-timeout=100000000
-        border-color=#fab387
+        border-color=#${base09}
       '';
     };
   };
