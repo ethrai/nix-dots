44a1ce403eece581c9e81a66b25075fc610ab739
Sergei Ryzhko <ryzhkosergej@gmail.com>
2024-10-06 16:24:11 +0300

    feat(nvim): update theme

--- nix-config/modules/apps/tui/nixvim/plugins/themes/rosepine.nix
+++ nix-config/modules/apps/tui/nixvim/plugins/themes/rosepine.nix
@@ -1,22 +1,24 @@
 {
-  colorschemes.rose-pine = {
-    enable = true;
-    settings = {
-      dark_variant = "moon";
-      dim_inactive_windows = true;
-      enable = {
-        legacy_highlights = false;
-        migrations = true;
-        terminal = false;
+  programs.nixvim = {
+    colorschemes.rose-pine = {
+      enable = true;
+      settings = {
+        dark_variant = "moon";
+        dim_inactive_windows = true;
+        enable = {
+          legacy_highlights = false;
+          migrations = true;
+          terminal = false;
+        };
+        extend_background_behind_borders = true;
+        highlight_groups = { };
+        styles = {
+          bold = true;
+          italic = false;
+          transparency = false;
+        };
+        variant = "auto";
       };
-      extend_background_behind_borders = true;
-      highlight_groups = { };
-      styles = {
-        bold = true;
-        italic = false;
-        transparency = false;
-      };
-      variant = "auto";
     };
   };
 }
